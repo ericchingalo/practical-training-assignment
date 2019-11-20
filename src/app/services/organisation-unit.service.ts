479cb20f190e10096dfaafb4d06cde8bbd295e31
chingaloEric <echingalo@gmail.com>
2019-11-20 08:07:54 +0300

    style: removed the typos

--- src/app/services/organisation-unit.service.ts
+++ src/app/services/organisation-unit.service.ts
@@ -1,30 +1,36 @@
-import { Injectable } from '@angular/core';
-import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
-import { Observable } from 'rxjs';
-import { OrganisationUnitChildren } from '../models/organisation-unit.model';
+import { Injectable } from "@angular/core";
+import { NgxDhis2HttpClientService } from "@iapps/ngx-dhis2-http-client";
+import { Observable } from "rxjs";
+import { OrganisationUnitChildren } from "../models/organisation-unit.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OrganisationUnitService {
-  constructor(private httpServuice: NgxDhis2HttpClientService) {}
+  constructor(private httpService: NgxDhis2HttpClientService) {}
   s;
   getOrgunitChildren(orgunitId: string): Observable<any> {
     const fields =
-      'id,name,lastUpdated,phoneNumber,level,displayName,displayShortName,openingDate,parent,path,coordinates,attributeValues[value,attribute[id,name]]';
-    return this.httpServuice.get(
+      "id,name,lastUpdated,phoneNumber,level,displayName,displayShortName,openingDate,parent,path,coordinates,attributeValues[value,attribute[id,name]]";
+    return this.httpService.get(
       `organisationUnits.json?fields=${fields}&filter=path:ilike:${orgunitId}&filter=level:eq:6&paging=false`
     );
   }
 
+  getReportingRate(): Observable<any> {
+    return this.httpService.get(
+      `analytics?dimension=dx:t6N3L1IElxb.ACTUAL_REPORTS&dimension=ou:LEVEL-6&dimension=pe:LAST_MONTH`
+    );
+  }
+
   editOrgunitChildren(orgunitChild: OrganisationUnitChildren): Observable<any> {
-    return this.httpServuice.put(
+    return this.httpService.put(
       `29/organisationUnits/${orgunitChild.id}?mergeMode=REPLACE`,
       orgunitChild
     );
   }
 
   deleteOrgunitChild(orgunitId): Observable<any> {
-    return this.httpServuice.delete(`organisationUnits/${orgunitId}`);
+    return this.httpService.delete(`organisationUnits/${orgunitId}`);
   }
 }
